3b231e66f687b0e2739325ed7a8a7f931d72f84a
carol <carol@10.0.0.241>
2025-02-15 09:19:13 -0500

    First Commit

---
+++ src/test/java/Homework1/Exercise1.java
@@ -0,0 +1,18 @@
+package Homework1;
+
+import utils.Hooks;
+
+public class Exercise1 extends Hooks {
+	
+	public static void main (String args[]) {
+		
+		
+		setUp("edge");
+		
+		
+		tearDown();
+		
+	}
+
+
+}
